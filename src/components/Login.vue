d4fc4c68c4597f1c24e7b135a07e162ef108ae5a
pongsakon <pongsakonpromma@gmail.com>
2019-04-22 13:42:09 +0700

    ลองเริ่มต้นโปรเจคเล่นๆ

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+
+<div class ="login">
+    <link rel="stylesheet" href="//cdn.rawgit.com/necolas/normalize.css/master/normalize.css">
+<h2>Essential Links</h2>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
